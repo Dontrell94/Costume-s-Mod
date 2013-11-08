f0bc1417367d030a218b66b8248860821f5bb836
Dontrell94 <fltlinr@gmail.com>
2013-11-07 18:06:36 -0800

    finishing code.

--- cm_source/costumeMod/armor/ArmorScarecrow.java
+++ cm_source/costumeMod/armor/ArmorScarecrow.java
@@ -1,6 +1,7 @@
 package costumeMod.armor;
 
 import costumeMod.Properties.ArmorProperties;
+import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.entity.Entity;
 import net.minecraft.item.EnumArmorMaterial;
 import net.minecraft.item.ItemArmor;
@@ -8,6 +9,9 @@ import net.minecraft.item.ItemStack;
 
 public class ArmorScarecrow extends ItemArmor
 {
+	public String helmandchest = "textures/models/armor/scarecrow_1.png";
+	public String bootsandlegs = "textures/models/armor/scarecrow_2.png";
+	
 	public ArmorScarecrow(int par1, EnumArmorMaterial par2EnumArmorMaterial, int par3, int par4)
 	{
 		super(par1, par2EnumArmorMaterial, par3, par4);
@@ -32,26 +36,27 @@ public class ArmorScarecrow extends ItemArmor
 		}
 		else return null;
 		}
+	
 		/**
 		* To Add Armor Texture to Item Armor (when held or in inventory)
 		*/
-		/*public void registerIcons(IconRegister iconReg) 
+		public void registerIcons(IconRegister iconReg) 
 		{
-		if(itemID == ArmorScarecrow.ScarecrowBoots.itemID)
+		if(itemID == ArmorProperties.ScarecrowBoots.itemID)
 		{
 			itemIcon = iconReg.registerIcon("costumemod:rubyboots");
 		}
-		else if(itemID == ArmorScarecrow.ScarecrowLeggings.itemID)
+		else if(itemID == ArmorProperties.ScarecrowLeggings.itemID)
 		{
 				itemIcon = iconReg.registerIcon("costumemod:rubypants");
 		}
-		else if(itemID == ArmorScarecrow.ScarecrowChestplate.itemID)
+		else if(itemID == ArmorProperties.ScarecrowChestplate.itemID)
 		{
 				itemIcon = iconReg.registerIcon("costumemod:rubychest");
 		}
-		else if(itemID == ArmorScarecrow.ScarecrowHelmet.itemID)
+		else if(itemID == ArmorProperties.ScarecrowHelmet.itemID)
 		{
 			itemIcon = iconReg.registerIcon("costumemod:rubyhelmet");
 		}
-	}*/
+	}
 }
